e4a2b8c8016beefd4862dc6cb8e9b2ed8945f9c6
unknown <785047870@qq.com>
2022-08-14 14:09:14 +0800

    导航配合View model使用，以及ViewModel与Fragment的搭配使用

---
+++ navigationdemo3/src/main/java/com/yett/navigationdemo3/MainActivity.java
@@ -0,0 +1,17 @@
+package com.yett.navigationdemo3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * 导航配合View model使用，以及ViewModel与Fragment的搭配使用
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
