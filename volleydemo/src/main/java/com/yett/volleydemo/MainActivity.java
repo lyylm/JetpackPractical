9bbe06dcfe51b3c163d42846dc3bc6ee3bdc6ba6
unknown <785047870@qq.com>
2022-08-22 22:18:41 +0800

    volley的简单使用

---
+++ volleydemo/src/main/java/com/yett/volleydemo/MainActivity.java
@@ -0,0 +1,55 @@
+package com.yett.volleydemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "lyy";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.textView);
+        String url = "https://www.jd.com";
+        RequestQueue queu = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(
+                StringRequest.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        textView.setText(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e(TAG, "onErrorResponse: ", error);
+                    }
+                }
+        );
+        queu.add(stringRequest);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
